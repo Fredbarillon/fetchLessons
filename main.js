2e4c4ec9eee467357f01d1e3606691030e2fa2eb
Fred <barillondev@gmail.com>
2025-04-18 15:29:48 +0200

    bonus 1 50%

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const form1 = document.querySelector("#form1");
 const form2 = document.querySelector("#form2");
 const inputName = document.querySelector("#name");
 const inputId = document.querySelector("#id");
-
+const list = document.querySelector("#generationList");
 const BASE_URL = "https://pokeapi.co/api/v2/";
 
 let targetPokemon = null;
@@ -24,6 +24,14 @@ async function getClosestPokemon(id, order) {
             const data = await response.json();
             return data;
 }
+
+async function getGeneration() {
+    const response = await fetch(BASE_URL + "generation");
+    const data = await response.json();
+    // console.log(data);
+    return data;
+    
+}
  
 const selectOtherPokemon = ()=> {
     const previous = document.querySelector("#btnPrevious");
@@ -36,7 +44,7 @@ const selectOtherPokemon = ()=> {
                 const order =-1;
                 const data = await getClosestPokemon(targetPokemon.id, order);
                 targetPokemon = storedPokemon(data);
-                console.log("previous:",targetPokemon);
+                // console.log("previous:",targetPokemon);
             }catch (error) {
                 console.error("Error fetching Pokemon :", error.message);
             }   
@@ -50,7 +58,7 @@ const selectOtherPokemon = ()=> {
                 const order = 1;
                 const data = await getClosestPokemon(targetPokemon.id, order);
                 targetPokemon = storedPokemon(data);
-                console.log("next", targetPokemon);
+                // console.log("next", targetPokemon);
             }catch (error) {
                 console.error("Error fetching Pokemon :", error.message);
             }   
@@ -84,6 +92,20 @@ const displayPokemon = (pokemon) => {
     table.appendChild(tr);
 }
 
+const displayList = async () => {
+    const data= await getGeneration();
+    const generationList = data.results;
+    console.log(generationList);
+    generationList.forEach((generation) => {
+        const option = document.createElement("option");
+        let i = 0;
+        option.value = i++;
+        option.textContent = generation.name;
+        list.appendChild(option);
+    });
+}
+displayList();
+
 const findPokemonByName = ()=> {
     form1.addEventListener("submit", async (event) => {
         event.preventDefault();
@@ -92,9 +114,7 @@ const findPokemonByName = ()=> {
             try {
                 const data = await getPokemon(name);
                 targetPokemon = storedPokemon(data);
-                console.log(targetPokemon);
-                
-                
+                // console.log(targetPokemon);   
             } catch (error) {
                 console.error("Error fetching Pokemon :", error.message);
             }
@@ -111,9 +131,7 @@ const findPokemonById = () => {
             try {
                 const data = await getPokemonById(id);
                 targetPokemon = storedPokemon(data);
-                console.log(targetPokemon);
-                
-                
+                // console.log(targetPokemon);   
             } catch (error) {
                 console.error("Error fetching Pokemon :", error.message);
             }
@@ -124,3 +142,32 @@ const findPokemonById = () => {
 findPokemonByName();
 findPokemonById();
 selectOtherPokemon();
+getGeneration();
+
+// Il devra être possible de pouvoir accéder à la liste des pokédex de génération, d'en sélectionner un pour ensuite se voir proposer directement les pokémon de cette génération
+list.addEventListener("click", async (event) => {
+    // const generationId = event.target.value;
+    // if (generationId) {
+    //     try {
+    //         const response = await fetch(BASE_URL + "generation/" + generationId);
+    //         const data = await response.json();
+    //         console.log(data);
+    //         // targetPokemon = storedGeneration(data);
+    //     } catch (error) {
+    //         console.error("Error fetching Generation :", error.message);
+    //     }
+    // }
+})
+
+// const generation = [];
+// function storedGeneration(generation) {
+//     const generation = pokemon.name;
+//     const weight = pokemon.weight;
+//     const height = pokemon.height;
+//     const types = pokemon.types.map(type => type.type.name).join(", ");
+//     const abilities = pokemon.abilities.map(ability => ability.ability.name).join(", ");
+//     const id = pokemon.id;
+//     targetPokemon = { name, weight, height, types, abilities, id };
+//     displayPokemon(targetPokemon);
+//     return targetPokemon
+//   }
